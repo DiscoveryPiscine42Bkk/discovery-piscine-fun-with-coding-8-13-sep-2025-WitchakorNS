8bb9fbc028e7bbcdf77ae9194900e620f31714c1
WitchakornNS <witchakron49@gmail.com>
2025-09-12 22:18:37 +0700

    Rust Project

---
+++ Rust/Witchakorn's Port/portfolio.js
@@ -0,0 +1,30 @@
+const burger = document.getElementById('burger');
+    const menu = document.getElementById('menu');
+
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('open');
+        if (menu.classList.contains('open')) {
+            burger.innerHTML = '✕';
+            burger.style.color = '#000000ff';
+        } else {
+            burger.innerHTML = '☰';
+            burger.style.color = '#000000ff';
+        }
+    });
+
+    const menuLinks = document.querySelectorAll('.menu a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('open');
+            burger.innerHTML = '☰';
+            burger.style.color = '#000000ff';
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!burger.contains(e.target) && !menu.contains(e.target)) {
+            menu.classList.remove('open');
+            burger.innerHTML = '☰';
+            burger.style.color = '#000000ff';
+        }
+    });
